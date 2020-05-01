a2f785aa3e1fd1edbf4243dac28fe2db64478cd5
Wilson <panweipei_2010@163.com>
2020-05-01 18:55:53 +0800

    Singleton Pattern

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# DesignPatterns
+# 设计模式
+
+设计模式 && C#代码实现
+
+## 创建型
+
+---
+
+- [单例模式（Singleton Pattern）](https://github.com/WilsonPan/DesignPatterns/Singleton/Overview.md)
+
+## 结构型
+
+---
+
+## 行为型
+
+---
---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(1, 50, (i, state) =>
+            {
+                var instance = Singleton.Instance;
+            });
+            Console.WriteLine("Finish");
+        }
+    }
+}
